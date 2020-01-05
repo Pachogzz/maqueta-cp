84d53fac6010129d9e60375b41620a7360ca88a4
Pacho Gonzalez <pachogzz@gmail.com>
2020-01-05 16:08:48 -0600

    Agregado secciones consideraciones tecnicas, faqs y auste en cpm en linea

---
+++ consideraciones-tecnicas-sei.php
@@ -0,0 +1,64 @@
+<?php include('inc/header.php'); ?>
+
+		<section class="after-flange header-title-inner  bg-gradient-purple">
+			<div class="container h-100">
+				<div class="row h-100 align-items-center">
+					<div class="col">
+						<h1 class="display-4 bg-primary d-inline-block px-lg-5 py-lg-3 br-30 text-white font-weight-semibold">Consideraciones técnicas</h1>
+					</div>
+				</div>
+			</div>
+		</section> <!-- /. Page Title -->
+
+		<?php include('inc/navs/inner-nav-page-cpm-en-linea.php') ?>
+
+		<section class="page-content">
+			<div class="container">
+				<div class="row mb-5">
+					<div class="col">
+						<blockquote class="blockquote p-5 border border-light bw-5 rounded border-grey1 text-center">
+							<p class="mb-0 h3 font-weight-normal">Conoce las consideraciones y requsitos técnicos para nuestras plataformas</p>
+						</blockquote>
+					</div>
+				</div> <!-- ./ Main info block -->
+
+				<div class="row">
+					<div class="col-12 text-center">
+						<h3>Consideraciones técnicas</h3>
+						<img src="assets/img/icons/icon_CPMMovil.png" class="bg-purple rounded-circle" alt="" width="120" height="120">
+						<h3>Consideraciones técnicas</h3>
+						<img src="assets/img/icons/icon_CPMMovil.png" class="bg-purple rounded-circle" alt="" width="120" height="120">
+					</div>
+				</div>
+
+			</div>
+		</section> <!-- ./ Page content -->
+
+		<!-- <section class="inner-nav-page secondary py-5 bg-white">
+			<div class="container">
+				<div class="row text-center">
+					<div class="col-12 col-lg p-0 mr-lg-n5 mb-2 mb-lg-0  z-index-4">
+						<a href="#" class="btn bg-tertiary rounded-pill btn-lg btn-block w-100 py-3 px-0 text-white">
+							Operaciones por plataforma
+						</a>
+					</div>
+					<div class="col-12 col-lg p-0 mr-lg-n5 mb-2 mb-lg-0  z-index-3">
+						<a href="#" class="btn bg-primary rounded-pill btn-lg btn-block w-100 py-3 px-0 text-white br-tl-0 br-bl-0">
+							Requisitos Técnicos
+						</a>
+					</div>
+					<div class="col-12 col-lg p-0 mr-lg-n5 mb-2 mb-lg-0  z-index-2">
+						<a href="#" class="btn bg-quaternary rounded-pill btn-lg btn-block w-100 py-3 px-0 text-white br-tl-0 br-bl-0">
+							Preguntas Frecuentes
+						</a>
+					</div>
+					<div class="col-12 col-lg p-0 mb-2 mb-lg-0 ">
+						<a href="#" class="btn bg-quinary rounded-pill btn-lg btn-block w-100 py-3 px-0 text-white br-tl-0 br-bl-0">
+							Disponibilidad del servicio
+						</a>
+					</div>
+				</div>
+			</div>
+		</section> --> <!-- /. Inner Nav Home -->
+
+<?php include('inc/footer.php'); ?>
---
+++ cpm-en-linea.php
@@ -0,0 +1,376 @@
+<?php include('inc/header.php'); ?>
+
+		<section class="after-flange header-title-inner  bg-gradient-purple">
+			<div class="container h-100">
+				<div class="row h-100 align-items-center">
+					<div class="col">
+						<h1 class="display-4 bg-primary d-inline-block px-lg-5 py-lg-3 br-30 text-white font-weight-semibold">CPM en línea</h1>
+					</div>
+				</div>
+			</div>
+		</section> <!-- /. Page Title -->
+
+		<?php include('inc/navs/inner-nav-page-cpm-en-linea.php') ?>
+
+		<section class="page-content">
+			<div class="container">
+				<div class="row mb-5">
+					<div class="col">
+						<blockquote class="blockquote p-5 border border-light bw-5 rounded border-grey1 text-center">
+							<p class="mb-0 lead font-weight-normal">Comineza a usar CPM en línea y comienza a tener más tiempo en tus manos</p>
+							<p class="mb-0 lead text-primary font-weight-bolder mb-3">
+								<a href="https://www.cpmenlinea.cpm.coop/" target="_blank">Ingresa aquí</a>
+							</p>
+							<p class="mt-3 mb-0 lead font-weight-normal">Consulta los términos y condiciones de nuestro servicio Electrónico por Internet dando <a href="#" target="_blank">clic aquí</a>.</p>
+						</blockquote>
+					</div>
+				</div> <!-- ./ Main info block -->
+
+				<div class="row video-tutorial-holder mb-5">
+					<div class="col-12 col-md-6 mb-5 mb-md-0">
+						<div class="embed-responsive embed-responsive-16by9">
+							<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/9i5BUEnW-_Q?rel=0" allowfullscreen></iframe>
+						</div>
+						<div class="video-label bg-purple p-3 text-center"><p class="h3 text-white font-weight-bold mb-0">Guía de primer ingreso</p></div>
+					</div>
+					<div class="col-12 col-md-6">
+						<div class="embed-responsive embed-responsive-16by9">
+							<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/FLdPlEdzkJU?rel=0" allowfullscreen></iframe>
+						</div>
+						<div class="video-label bg-purple p-3 text-center"><p class="h3 text-white font-weight-bold mb-0">Guía de transferencias</p></div>
+					</div>
+				</div> <!-- ./ Video guides -->
+
+				<div class="row mb-5">
+					<div class="col-12 col-md-6 offset-md-3 text-center py-3">
+						<h3 class="title-medium font-weight-bold">GUÍAS DE USO</h3>
+						<p class="lead font-weight-semibold">Selecciona la guía de tu interés por su categoría</p>
+						<form action="">
+							<div class="form-group">
+								<select class="form-control" id="filterSelector">
+									<option value="seleccione">- Seleccione una guía -</option>
+									<option value="0">Guía de uso general</option>
+									<option value="1">Guía primer ingreso</option>
+									<option value="2">Guía de transacciones</option>
+									<option value="3">Guía de consulta y eliminación de transferencias programadas</option>
+									<option value="4">Guía de transferencias a cuentas de otros socios</option>
+									<option value="5">Guía de administración de dispositivo</option>
+									<option value="6">Guía de consulta de saldos</option>
+									<option value="7">Guía de consulta, bloqueo y desbloqueo de tarjeta</option>
+									<option value="8">Guía de notificaciones de cuentas</option>
+									<option value="9">Guía de administración de terceros</option>
+									<option value="10">Guía de consulta de actividad en línea y recibo electrónico</option>
+									<option value="11">Guía de cambio de nombre de usuario</option>
+									<option value="12">Guía de administración de límites diarios</option>
+									<option value="13">Guía de cambio de contraseña</option>
+									<option value="14">Guía de envío y respuesta de mensajes del servicio</option>
+								</select>
+							</div>
+						</form>
+					</div>
+				</div>
+
+				<div class="row guides-grid">
+				  <div id="0" class="guides-grid-col col-12 col-md-4">
+				    <div class="card h-100 guides-grid-block">
+				      <img src="assets/img/icons/icon-pdf.png" class="" width="100" height="100">
+				      <div class="card-body">
+				        <h4 class="card-title mb-3 font-weight-bolder">Guía de uso general</h4>
+				      </div>
+				      <div class="card-footer">
+								<div class="row">
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center">Visualizar</a>
+									</div>
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center" download>Descargar</a>
+									</div>
+								</div>
+				      </div>
+				    </div>
+				  </div>
+				  <div id="1" class="guides-grid-col col-12 col-md-4">
+				    <div class="card h-100 guides-grid-block">
+				      <img src="assets/img/icons/icon-pdf.png" class="" width="100" height="100">
+				      <div class="card-body">
+				        <h4 class="card-title mb-3 font-weight-bolder">Guía primer ingreso</h4>
+				      </div>
+				      <div class="card-footer">
+								<div class="row">
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center">Visualizar</a>
+									</div>
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center" download>Descargar</a>
+									</div>
+								</div>
+				      </div>
+				    </div>
+				  </div>
+				  <div id="2" class="guides-grid-col col-12 col-md-4">
+				    <div class="card h-100 guides-grid-block">
+				      <img src="assets/img/icons/icon-pdf.png" class="" width="100" height="100">
+				      <div class="card-body">
+				        <h4 class="card-title mb-3 font-weight-bolder">Guía de transacciones</h4>
+				      </div>
+				      <div class="card-footer">
+								<div class="row">
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center">Visualizar</a>
+									</div>
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center" download>Descargar</a>
+									</div>
+								</div>
+				      </div>
+				    </div>
+				  </div>
+				  <div id="3" class="guides-grid-col col-12 col-md-4">
+				    <div class="card h-100 guides-grid-block">
+				      <img src="assets/img/icons/icon-pdf.png" class="" width="100" height="100">
+				      <div class="card-body">
+				        <h4 class="card-title mb-3 font-weight-bolder">Guía de consulta y eliminación de transferencias programadas</h4>
+				      </div>
+				      <div class="card-footer">
+								<div class="row">
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center">Visualizar</a>
+									</div>
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center" download>Descargar</a>
+									</div>
+								</div>
+				      </div>
+				    </div>
+				  </div>
+				  <div id="4" class="guides-grid-col col-12 col-md-4">
+				    <div class="card h-100 guides-grid-block">
+				      <img src="assets/img/icons/icon-pdf.png" class="" width="100" height="100">
+				      <div class="card-body">
+				        <h4 class="card-title mb-3 font-weight-bolder">Guía de transferencias a cuentas de otros socios</h4>
+				      </div>
+				      <div class="card-footer">
+								<div class="row">
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center">Visualizar</a>
+									</div>
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center" download>Descargar</a>
+									</div>
+								</div>
+				      </div>
+				    </div>
+				  </div>
+				  <div id="5" class="guides-grid-col col-12 col-md-4">
+				    <div class="card h-100 guides-grid-block">
+				      <img src="assets/img/icons/icon-pdf.png" class="" width="100" height="100">
+				      <div class="card-body">
+				        <h4 class="card-title mb-3 font-weight-bolder">Guía de administración de dispositivo</h4>
+				      </div>
+				      <div class="card-footer">
+								<div class="row">
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center">Visualizar</a>
+									</div>
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center" download>Descargar</a>
+									</div>
+								</div>
+				      </div>
+				    </div>
+				  </div>
+				  <div id="6" class="guides-grid-col col-12 col-md-4">
+				    <div class="card h-100 guides-grid-block">
+				      <img src="assets/img/icons/icon-pdf.png" class="" width="100" height="100">
+				      <div class="card-body">
+				        <h4 class="card-title mb-3 font-weight-bolder">Guía de consulta de saldos</h4>
+				      </div>
+				      <div class="card-footer">
+								<div class="row">
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center">Visualizar</a>
+									</div>
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center" download>Descargar</a>
+									</div>
+								</div>
+				      </div>
+				    </div>
+				  </div>
+				  <div id="7" class="guides-grid-col col-12 col-md-4">
+				    <div class="card h-100 guides-grid-block">
+				      <img src="assets/img/icons/icon-pdf.png" class="" width="100" height="100">
+				      <div class="card-body">
+				        <h4 class="card-title mb-3 font-weight-bolder">Guía de consulta, bloqueo y desbloqueo de tarjeta</h4>
+				      </div>
+				      <div class="card-footer">
+								<div class="row">
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center">Visualizar</a>
+									</div>
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center" download>Descargar</a>
+									</div>
+								</div>
+				      </div>
+				    </div>
+				  </div>
+				  <div id="8" class="guides-grid-col col-12 col-md-4">
+				    <div class="card h-100 guides-grid-block">
+				      <img src="assets/img/icons/icon-pdf.png" class="" width="100" height="100">
+				      <div class="card-body">
+				        <h4 class="card-title mb-3 font-weight-bolder">Guía de notificaciones de cuentas</h4>
+				      </div>
+				      <div class="card-footer">
+								<div class="row">
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center">Visualizar</a>
+									</div>
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center" download>Descargar</a>
+									</div>
+								</div>
+				      </div>
+				    </div>
+				  </div>
+				  <div id="9" class="guides-grid-col col-12 col-md-4">
+				    <div class="card h-100 guides-grid-block">
+				      <img src="assets/img/icons/icon-pdf.png" class="" width="100" height="100">
+				      <div class="card-body">
+				        <h4 class="card-title mb-3 font-weight-bolder">Guía de administración de terceros</h4>
+				      </div>
+				      <div class="card-footer">
+								<div class="row">
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center">Visualizar</a>
+									</div>
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center" download>Descargar</a>
+									</div>
+								</div>
+				      </div>
+				    </div>
+				  </div>
+				  <div id="10" class="guides-grid-col col-12 col-md-4">
+				    <div class="card h-100 guides-grid-block">
+				      <img src="assets/img/icons/icon-pdf.png" class="" width="100" height="100">
+				      <div class="card-body">
+				        <h4 class="card-title mb-3 font-weight-bolder">Guía de consulta de actividad en línea y recibo electrónico</h4>
+				      </div>
+				      <div class="card-footer">
+								<div class="row">
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center">Visualizar</a>
+									</div>
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center" download>Descargar</a>
+									</div>
+								</div>
+				      </div>
+				    </div>
+				  </div>
+				  <div id="11" class="guides-grid-col col-12 col-md-4">
+				    <div class="card h-100 guides-grid-block">
+				      <img src="assets/img/icons/icon-pdf.png" class="" width="100" height="100">
+				      <div class="card-body">
+				        <h4 class="card-title mb-3 font-weight-bolder">Guía de cambio de nombre de usuario</h4>
+				      </div>
+				      <div class="card-footer">
+								<div class="row">
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center">Visualizar</a>
+									</div>
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center" download>Descargar</a>
+									</div>
+								</div>
+				      </div>
+				    </div>
+				  </div>
+				  <div id="12" class="guides-grid-col col-12 col-md-4">
+				    <div class="card h-100 guides-grid-block">
+				      <img src="assets/img/icons/icon-pdf.png" class="" width="100" height="100">
+				      <div class="card-body">
+				        <h4 class="card-title mb-3 font-weight-bolder">Guía de administración de límites diarios</h4>
+				      </div>
+				      <div class="card-footer">
+								<div class="row">
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center">Visualizar</a>
+									</div>
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center" download>Descargar</a>
+									</div>
+								</div>
+				      </div>
+				    </div>
+				  </div>
+				  <div id="13" class="guides-grid-col col-12 col-md-4">
+				    <div class="card h-100 guides-grid-block">
+				      <img src="assets/img/icons/icon-pdf.png" class="" width="100" height="100">
+				      <div class="card-body">
+				        <h4 class="card-title mb-3 font-weight-bolder">Guía de cambio de contraseña</h4>
+				      </div>
+				      <div class="card-footer">
+								<div class="row">
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center">Visualizar</a>
+									</div>
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center" download>Descargar</a>
+									</div>
+								</div>
+				      </div>
+				    </div>
+				  </div>
+				  <div id="14" class="guides-grid-col col-12 col-md-4">
+				    <div class="card h-100 guides-grid-block">
+				      <img src="assets/img/icons/icon-pdf.png" class="" width="100" height="100">
+				      <div class="card-body">
+				        <h4 class="card-title mb-3 font-weight-bolder">Guía de envío y respuesta de mensajes del servicio</h4>
+				      </div>
+				      <div class="card-footer">
+								<div class="row">
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center">Visualizar</a>
+									</div>
+									<div class="col-6">
+										<a href="#" class="btn btn-md btn-block rounded-pill bg-purple text-white text-center" download>Descargar</a>
+									</div>
+								</div>
+				      </div>
+				    </div>
+				  </div>
+				</div>
+
+			</div>
+		</section> <!-- ./ Page content -->
+
+		<!-- <section class="inner-nav-page secondary py-5 bg-white">
+			<div class="container">
+				<div class="row text-center">
+					<div class="col-12 col-lg p-0 mr-lg-n5 mb-2 mb-lg-0  z-index-4">
+						<a href="#" class="btn bg-tertiary rounded-pill btn-lg btn-block w-100 py-3 px-0 text-white">
+							Operaciones por plataforma
+						</a>
+					</div>
+					<div class="col-12 col-lg p-0 mr-lg-n5 mb-2 mb-lg-0  z-index-3">
+						<a href="#" class="btn bg-primary rounded-pill btn-lg btn-block w-100 py-3 px-0 text-white br-tl-0 br-bl-0">
+							Requisitos Técnicos
+						</a>
+					</div>
+					<div class="col-12 col-lg p-0 mr-lg-n5 mb-2 mb-lg-0  z-index-2">
+						<a href="#" class="btn bg-quaternary rounded-pill btn-lg btn-block w-100 py-3 px-0 text-white br-tl-0 br-bl-0">
+							Preguntas Frecuentes
+						</a>
+					</div>
+					<div class="col-12 col-lg p-0 mb-2 mb-lg-0 ">
+						<a href="#" class="btn bg-quinary rounded-pill btn-lg btn-block w-100 py-3 px-0 text-white br-tl-0 br-bl-0">
+							Disponibilidad del servicio
+						</a>
+					</div>
+				</div>
+			</div>
+		</section> --> <!-- /. Inner Nav Home -->
+
+<?php include('inc/footer.php'); ?>
